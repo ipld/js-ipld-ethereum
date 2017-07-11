eebb817836d739e901d0aa4b6717ba20f8fc04a4
kumavis <aaron@kumavis.me>
2017-07-10 23:25:41 -0700

    add entrypoint

---
+++ src/index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+module.exports = {
+  ethAccountSnapshot: require('../eth-account-snapshot'),
+  ethBlock: require('../eth-block'),
+  ethBlockList: require('../eth-block-list'),
+  ethStateTrie: require('../eth-state-trie'),
+  ethStorageTrie: require('../eth-storage-trie'),
+  ethTx: require('../eth-tx'),
+  ethTxTrie: require('../eth-tx-trie')
+}
